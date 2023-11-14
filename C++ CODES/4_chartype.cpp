355e6662beda22d8787cb7bbc8ab308a74f0afd5
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-11-14 22:21:06 +0530

    update

---
+++ C++ CODES/4_chartype.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // char only takes only a single character
+    char name ;
+    cout << "enter your name : " << endl;
+    cin >> name ;
+    cout << " Hi " << name << " !" << endl;
+    return 0;
+}
