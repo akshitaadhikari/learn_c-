7267a98a1cc0b69cb281b72bddf6690b10d0b2db
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-11-25 18:08:22 +0530

    Create 15_bondini.cpp

---
+++ C++ CODES/15_bondini.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"\a Operation \" HyperHype \" is now activated \n";
+    cout << " Enter your agent code : ______\b\b\b\b\b\b";
+    long code;
+    cin >> code ;
+    cout << " You entered " << code << ".....\n";
+    cout << "\a Code verified " << "\a proceed with plan Z3\n";
+    return 0;
+}
