6de1e7f7ca46fdd4ee9590f2e19ea8bac90433f7
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-11-30 19:23:58 +0530

    Create 20_hexoct2.cpp

---
+++ C++ CODES/20_hexoct2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height = 145 ;
+    int weight = 145 ;
+    int age = 55 ;
+
+    cout << "height in decimals = " << height << endl;
+    cout << hex ;
+    cout << "weight in hexadecimals = " << weight << endl;
+    cout << oct ;
+    cout << "age in octal = " << age << endl;
+    return 0;
+
+}
