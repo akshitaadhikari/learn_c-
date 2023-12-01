de7e03c104d95cdb87e4f9534efb717ea987f339
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-12-01 23:40:39 +0530

    Create 21_hexooct1.cpp

---
+++ C++ CODES/21_hexooct1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 56;
+    int b = 046;
+    int c = 0xd4;
+
+    cout << " a in decimals = " << a << endl;
+    cout << " b in decimals = " << b << endl;
+    cout << " c in decimals = " << c << endl;
+    return 0;
+
+}
