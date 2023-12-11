213488454046db9981ef4d4782e8e8e0908cc6e7
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-12-11 17:08:01 +0530

    Create 31_sqrt.cpp

---
+++ C++ CODES/31_sqrt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+int main()
+{
+    
+    cout << "enter the area" << endl;
+    double x;
+    cin >> x ;
+    //double y;
+    //y = sqrt(x) ;
+    //or
+    //double y = sqrt(x);
+    cout << " side of the room is " << sqrt(x) << endl ;
+    return 0 ;
+}
